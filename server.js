6120b72a3922ef6cf628d1958d06a28d037c39f6
管天元 <guantianyuan@guantianyuandeMacBook-Pro.local>
2018-05-02 11:16:57 -0700

    cloned s3Eg

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8080;
+var port = 1234;
 
 var Client = require('node-rest-client').Client;
 
